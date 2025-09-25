f07ff182904de0dda37a3191283d4120cc56a0ce
AbhiBagde02 <bagdeabhijeet2@gmail.com>
2025-09-25 19:44:24 +0530

    changes for adding test of supporting multiple long delimiters

--- src/test/java/com/incubyte/kata/StringCalculatorTest.java
+++ src/test/java/com/incubyte/kata/StringCalculatorTest.java
@@ -55,4 +55,8 @@ public class StringCalculatorTest {
         assertEquals(6, StringCalculator.add("//[*][%]\n1*2%3"));
     }
 
+    @Test
+    void supportsMultipleLongDelimiters() {
+        assertEquals(6, StringCalculator.add("//[***][%%]\n1***2%%3"));
+    }
 }
