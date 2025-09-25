7a1d90d7a9b319c97e2c681bb32f33eb403242c4
AbhiBagde02 <bagdeabhijeet2@gmail.com>
2025-09-25 17:46:57 +0530

    changes for returning sum of two numbers

--- src/main/java/com/incubyte/kata/StringCalculator.java
+++ src/main/java/com/incubyte/kata/StringCalculator.java
@@ -1,10 +1,13 @@
 package com.incubyte.kata;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public static int add(String  numbers){
        if(numbers.isEmpty()) return 0;
-       return Integer.parseInt(numbers);
+       String[] parts = numbers.split(",");
+        return Arrays.stream(parts).mapToInt(Integer::parseInt).sum();
     }
 
 }
--- src/test/java/com/incubyte/kata/StringCalculatorTest.java
+++ src/test/java/com/incubyte/kata/StringCalculatorTest.java
@@ -16,4 +16,8 @@ public class StringCalculatorTest {
         assertEquals(1,StringCalculator.add("1"));
     }
 
+    @Test
+    void returnSumOfTwoNumbers(){
+        assertEquals(3,StringCalculator.add("1,2"));
+    }
 }
